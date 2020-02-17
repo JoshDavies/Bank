7943dbc7e664da1a44d9b908fc7b11da877401a2
JoshDavies <jdaviescreative@gmail.com>
2020-02-17 17:47:35 +0000

    user can withdraw an ammount from their account

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -13,8 +13,15 @@ describe('Account', () => {
   describe('deposit', () => {
     it('should add a given amount to the users total balance', () =>{
       expect(account.deposit(1000)).toEqual("1000.00")
-      expect(account.deposit(1000)).toEqual("2000.00")
+      expect(account.deposit(2000)).toEqual("3000.00")
     })
-  })
+  });
+
+  describe('withdraw', () => {
+    it('should subtract a given amount from the users total balance', () =>{
+      account.balance = 3000
+      expect(account.withdraw(500)).toEqual("2500.00")
+    })
+  });
 
 });
--- src/Account.js
+++ src/Account.js
@@ -7,8 +7,13 @@ class Account {
     return this.balance.toFixed(2);
   }
 
-  deposit(amount) {
-    this.balance += amount;
-    return this.balance.toFixed(2);
+  deposit(credit) {
+    this.balance += credit;
+    return this.currentBalance();
+  }
+
+  withdraw(debit) {
+    this.balance -= debit;
+    return this.currentBalance();
   }
 };
